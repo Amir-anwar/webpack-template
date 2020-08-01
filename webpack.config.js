be34c04bbb156d29e6b4c338be5277fa5ded941b
Amir-anwar <super4amir@gmail.com>
2020-08-01 15:02:40 +0200

    cleaning

--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,18 @@ module.exports = {
           'file-loader',
         ],
       },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader',
+        ],
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader',
+        ],
+      },
     ],
   },
 };
